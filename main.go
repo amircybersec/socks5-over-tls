cb2263f5458969053579311f4f102d8994680b8c
amir <amir@we-pn.com>
2024-04-04 16:46:31 -0700

    add listening address to cmd

--- main.go
+++ main.go
@@ -1,19 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net"
 	"os"
 
 	"github.com/things-go/go-socks5"
 )
 
 func main() {
-	if len(os.Args) < 3 {
-		log.Fatal("Usage: go run main.go <username> <password>")
+	if len(os.Args) < 5 {
+		log.Fatal("Usage: go run main.go <username> <password> <listening_address> <listening_port>")
 	}
 
 	username := os.Args[1]
 	password := os.Args[2]
+	listeningAddress := os.Args[3]
+	listeningPort := os.Args[4]
+
+	// Validate listening address
+	if ip := net.ParseIP(listeningAddress); ip == nil {
+		log.Fatalf("Invalid listening address: %s", listeningAddress)
+	}
+
+	// Validate listening port
+	if port, err := net.LookupPort("tcp", listeningPort); err != nil {
+		log.Fatalf("Invalid listening port: %s", listeningPort)
+	} else if port < 1 || port > 65535 {
+		log.Fatalf("Invalid listening port: %d", port)
+	}
 
 	cator := socks5.UserPassAuthenticator{
 		socks5.StaticCredentials{
@@ -26,8 +42,9 @@ func main() {
 		socks5.WithLogger(socks5.NewLogger(log.New(os.Stdout, "socks5: ", log.LstdFlags))),
 	)
 
-	// Create SOCKS5 proxy on localhost port 8000
-	if err := server.ListenAndServe("tcp", ":8080"); err != nil {
+	// Create SOCKS5 proxy on the specified address and port
+	listeningAddr := fmt.Sprintf("%s:%s", listeningAddress, listeningPort)
+	if err := server.ListenAndServe("tcp", listeningAddr); err != nil {
 		panic(err)
 	}
 
